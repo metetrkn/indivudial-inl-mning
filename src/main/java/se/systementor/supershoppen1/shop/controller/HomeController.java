c42acf1bedf07e19c7bc273c76b75af83cce2360
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-11-06 10:42:35 +0100

    first commit

---
+++ src/main/java/se/systementor/supershoppen1/shop/controller/HomeController.java
@@ -0,0 +1,33 @@
+package se.systementor.supershoppen1.shop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import se.systementor.supershoppen1.shop.model.Product;
+import se.systementor.supershoppen1.shop.services.ProductService;
+
+@Controller
+public class HomeController {
+    private  ProductService productService;
+    @Autowired
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }    
+
+    @GetMapping(path="/")
+    String empty(Model model)
+    {
+        return "test";
+    }
+
+    @GetMapping(path="/test2")
+    List<Product> getAll(){
+        return productService.getAll();
+    }
+
+
+}
