02a643aceb1b1aea48e9b5f0d4f2d6c8946a5ed1
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-12-03 15:40:02 +0100

    Added seeddata bug

--- src/main/java/se/systementor/supershoppen1/shop/SeedData.java
+++ src/main/java/se/systementor/supershoppen1/shop/SeedData.java
@@ -173,7 +173,7 @@ public class SeedData implements CommandLineRunner {
     }
     private void addProduct(List<Product> existing, int catId, String name,int pris,int stocklevel,String description){
         for (Product product : existing) {
-            if (product.getName().equals(name)) return;
+            if (product.getName() == name) return;
         }
         Product product1 = new Product();
         product1.setName(name);
